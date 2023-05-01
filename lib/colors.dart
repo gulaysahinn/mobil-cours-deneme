48a6e05ba11beb923d828580d8d1011529af9696
gulaysahinn <gulaysahin171@gmail.com>
2023-05-01 16:22:53 +0300

    new project

---
+++ lib/colors.dart
@@ -0,0 +1,16 @@
+import 'dart:ui';
+class AppColor{
+  static Color homePageBackground=const Color(0xFFfbfcff);
+  static Color homePageTitle=const Color(0xFF000000);
+  static Color homePageIcon=const Color(0xFF282022);
+  static Color homePageSubtitle=const Color(0xFF161617);
+  static Color homePageDetails=const Color(0xFF5bc1e8);
+  static Color gradientFirst=const Color(0xFF1f1fdb);
+  static Color gradientSecond=const Color(0xFF8f92ef);
+  static Color homePageContainerTextSmall=const Color(0xFFffffff);
+  static Color homePagePlayerIcon=const Color(0xFFffffff);
+  static Color homePageTitleSecond=const Color(0xFFffffff);
+  static Color homePageCard=const Color(0xFF1641d4);
+  static Color homePageCardText=const Color(0xFFcdcdca);
+
+}
